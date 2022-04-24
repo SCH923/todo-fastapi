5c399a7010630c07702416419c89b8ac090d0996
Shogo Naito <mimi_giza@yahoo.co.jp>
2022-04-25 03:33:08 +0900

    fastAPIのGET,POST,PUT,DELETE実装

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 app = FastAPI()
 
 class Task(BaseModel):
+    id: int
     text: str
     done: bool
     
@@ -40,14 +41,33 @@ def create(task: Task):
 
 
 @app.put("/")
-def update():
-    return {"message": "Hello World"}
+def update(new_task_id:int, new_task:Task):
+
+    json_load = []
+    new_task_dict = new_task.dict()
+    
+    if os.path.isfile('todo.json'):
+        with open('todo.json', 'r') as json_file:
+            json_load = json.load(json_file)
+    else:
+        raise Exception("todo.json not found")
+    
+    for index, task in enumerate(json_load):
+        if task["id"] == new_task_id:
+            json_load[index] = new_task_dict
+    
+    with open('todo.json', 'w') as json_file:
+        json.dump(json_load, json_file)
+             
+    return json_load
 
 
 @app.delete("/")
 def delete(task:Task):
+
     json_load = []
     task_dict = task.dict()
+    
     if os.path.isfile('todo.json'):
         with open('todo.json', 'r') as json_file:
             json_load = json.load(json_file)
